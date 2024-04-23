b9af939e8c7ad009f76b60d1d3434efd20aeb5bb
huynvn <Huy.Nguyen@navigosgroup.com>
2024-04-23 10:06:22 +0700

    Update code create and delete task

--- zustand-2/src/components/Column.jsx
+++ zustand-2/src/components/Column.jsx
@@ -1,13 +1,59 @@
-import React from "react";
+import { useState } from "react";
 import "./Colunm.css";
 import Task from "./Task";
+import { useStore } from "../store";
 
 // eslint-disable-next-line react/prop-types
 export default function Column({ state }) {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const tasks = useStore((store) =>
+    store.tasks.filter((task) => task.state === state)
+  );
+
+  const addTask = useStore((store) => store.addTask);
+
   return (
-    <div className="column">
-      <p>{state}</p>
-      <Task title="Todo" />
+    <div
+      className='column'
+      onDragOver={(e) => {
+        e.preventDefault();
+      }}
+      onDrop={(e) => {
+        console.log("Drop");
+      }}
+    >
+      <div className='titleWrapper'>
+        <p>{state}</p>
+        <button
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          Add Task
+        </button>
+      </div>
+
+      {tasks.map((task) => (
+        <Task key={task.title} title={task.title} />
+      ))}
+      {open && (
+        <div className='Modal'>
+          <div className='modalContent'>
+            <input onChange={(e) => setText(e.target.value)} value={text} />
+            <button
+              onClick={() => {
+                addTask(text, state);
+                setText("");
+                setOpen(false);
+              }}
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
---
+++ zustand-2/src/store.js
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+const store = (set) => ({
+  tasks: [{ title: "Test Task", state: "PLANNED" }],
+  addTask: (title, state) =>
+    set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+  deleteTask: (title) =>
+    set((store) => ({
+      tasks: store.tasks.filter((task) => task.title !== title),
+    })),
+});
+
+export const useStore = create(store);
