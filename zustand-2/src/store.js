734c400e8af7e04bd6dc389ed16a579fe334a011
huynvn <Huy.Nguyen@navigosgroup.com>
2024-05-05 08:57:54 +0700

    Done Zustand-2

--- zustand-2/src/store.js
+++ zustand-2/src/store.js
@@ -1,10 +1,20 @@
 import { create } from "zustand";
+import { devtools, persist, subscribeWithSelector } from "zustand/middleware";
+import produce from "immer";
 
 const store = (set) => ({
-  tasks: [{ title: "Test Task", state: "PLANNED" }],
+  tasks: [],
   draggedTask: null,
+  tasksInOngoing: 0,
   addTask: (title, state) =>
-    set((store) => ({ tasks: [...store.tasks, { title, state }] })),
+    set(
+      // (store) => ({ tasks: [...store.tasks, { title, state }] }),
+      produce((store) => {
+        store.tasks.push({ title, state });
+      }),
+      false,
+      "addTask"
+    ),
   deleteTask: (title) =>
     set((store) => ({
       tasks: store.tasks.filter((task) => task.title !== title),
@@ -18,4 +28,26 @@ const store = (set) => ({
     })),
 });
 
-export const useStore = create(store);
+const log = (config) => (set, get, api) =>
+  config(
+    (...args) => {
+      console.log("args", args);
+      set(...args);
+    },
+    get,
+    api
+  );
+
+export const useStore = create(
+  subscribeWithSelector(log(persist(devtools(store), { name: "store" })))
+);
+
+useStore.subscribe(
+  (store) => store.tasks,
+  (newTasks, prevTasks) => {
+    useStore.setState({
+      tasksInOngoing: newTasks.filter((task) => task.state === "ONGOING")
+        .length,
+    });
+  }
+);
